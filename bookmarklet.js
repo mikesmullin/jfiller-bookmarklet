2e5a45f1cc81baa1f65c06a232dd0752fe88f136
Mike Smullin <mike@smullindesign.com>
2012-05-15 13:53:23 -0600

    bookmarklet detects http vs https

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,4 +1,4 @@
-var basepath = 'http://mikesmullin.github.com/jfiller-bookmarklet/jfiller/';
+var basepath = window.location.protocol +'/mikesmullin.github.com/jfiller-bookmarklet/jfiller/';
 jQuery('head').append('<link rel="stylesheet" href="'+ basepath +'jfiller.css" type="text/css" />');
 jQuery.getScript(basepath +'jquery.cookie.js', function() {
   jQuery.getScript(basepath +'jfiller.js', function() {
